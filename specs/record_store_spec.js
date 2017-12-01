3371b7337c6c3f445580a33e823a05d0c3eb67af
Peter McCready <petermccready2@gmail.com>
2017-12-01 16:23:43 +0000

    Sets up Record Store constructor with Balance property

--- specs/record_store_spec.js
+++ specs/record_store_spec.js
@@ -6,7 +6,7 @@ describe("record store tests", function(){
   var recordStore;
 
   beforeEach(function(){
-    recordStore = new RecordStore("Big Pals", "Glasgow");
+    recordStore = new RecordStore("Big Pals", "Glasgow", 20000);
   });
 
   it('should have a Name', function(){
@@ -21,6 +21,8 @@ describe("record store tests", function(){
     assert.strictEqual(recordStore.inventory.length, 0);
   });
 
-  it('should have a balance');
+  it('should have a balance', function(){
+    assert.strictEqual(recordStore.balance, 20000);
+  });
 
 });
