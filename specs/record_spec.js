8198b250bdbb30ee5aa5aa97404d9f689c90b9f2
Peter McCready <petermccready2@gmail.com>
2017-12-01 15:15:00 +0000

    Adds pending record tests

---
+++ specs/record_spec.js
@@ -0,0 +1,21 @@
+var assert = require('assert');
+var Record = require('../record.js');
+
+
+describe('record tests', function(){
+
+  var record;
+
+  beforeEach(function(){
+    record = new Record("Young Guns", "Bones", "Rock", 7.99);
+  });
+
+  it('should have an artist');
+
+  it('should have a title');
+
+  it('should have a genre');
+
+  it('should have a price');
+
+});
