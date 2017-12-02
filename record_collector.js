e5b505e283c53d35ff6d06aec14a9219b19ede70
Peter McCready <petermccready2@gmail.com>
2017-12-02 21:22:01 +0000

    Sets up Cash property in Record Collector constructor

--- record_collector.js
+++ record_collector.js
@@ -1,16 +1,19 @@
-var RecordCollector = function(){
+var RecordCollector = function(cash){
   this.collection = [];
+  this.cash = cash;
 }
 
 RecordCollector.prototype = {
 
   buyRecord: function(recordStore, record){
     this.collection.push(recordStore.sellRecord(record));
+    this.cash -= record.price;
   },
 
   sellRecord: function(recordStore, record){
     var index = this.collection.lastIndexOf(record);
     if (index !== -1){
+      this.cash += record.price;
       return this.collection.splice(index, 1);
     }
   }
--- specs/record_collector_spec.js
+++ specs/record_collector_spec.js
@@ -13,7 +13,7 @@ describe("record collector tests", function(){
   var record3;
 
   beforeEach(function(){
-    recordCollector = new RecordCollector();
+    recordCollector = new RecordCollector(20);
     recordStore = new RecordStore("Big Pals", "Glasgow", 20000);
     record = new Record("Young Guns", "Bones", "Rock", 7.99);
     record2 = new Record("Foo Fighters", "One By One", "Rock", 6.95);
@@ -41,6 +41,15 @@ describe("record collector tests", function(){
     assert.strictEqual(recordStore.inventory.length, 3);
   });
 
-  it('should have cash that increase and decreases with buying and selling');
+  it('should have cash that increases and decreases with buying and selling respectively', function(){
+    assert.strictEqual(recordCollector.cash, 20);
+    recordCollector.buyRecord(recordStore, record);
+
+    assert.strictEqual(recordCollector.cash, 12.01);
+    
+    recordStore.addRecord(recordCollector.sellRecord(recordStore, record));
+
+    assert.strictEqual(recordCollector.cash, 20);
+  });
 
 });
