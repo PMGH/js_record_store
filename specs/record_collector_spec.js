43ae7f51cf629649572864bf85cfa98ef4c93b24
Peter McCready <petermccready2@gmail.com>
2017-12-02 20:25:58 +0000

    Adds pending RecordCollector tests

---
+++ specs/record_collector_spec.js
@@ -0,0 +1,17 @@
+var assert = require('assert');
+var RecordCollector = require('../record_collector.js');
+
+
+describe("record collector tests", function(){
+
+  var recordCollector;
+
+  beforeEach(function(){
+    recordCollector = new RecordCollector();
+  });
+
+  it('should be able to buy records');
+
+  it('should be able to sell records');
+
+});
